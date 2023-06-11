78042266353c36f87898ec495ad4aaed53694886
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-06-11 14:21:59 +0530

    resume update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,7 @@ import { ArrowRightCircle } from 'react-bootstrap-icons';
 // import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
+
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
@@ -15,18 +16,18 @@ export const Banner = () => {
 
 
   useEffect(() => {
-    const toRotate = [ "Full Stack", "Competitive Programming", "Data Science" ];
+    const toRotate = ["Full Stack", "Competitive Programming", "Data Science"];
     const tick = () => {
       let i = loopNum % toRotate.length;
       let fullText = toRotate[i];
       let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-  
+
       setText(updatedText);
-  
+
       if (isDeleting) {
         setDelta(prevDelta => prevDelta / 2);
       }
-  
+
       if (!isDeleting && updatedText === fullText) {
         setIsDeleting(true);
         setDelta(period);
@@ -41,7 +42,7 @@ export const Banner = () => {
     }, delta);
 
     return () => { clearInterval(ticker) };
-  }, [delta,isDeleting,loopNum,text.length])
+  }, [delta, isDeleting, loopNum, text.length])
 
 
 
@@ -52,24 +53,36 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>Hi! My Name is Rudhram Saraswat</h1>
-                <h1>{`I'm into `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack", "Competitive Programming", "Data Science" ]'><span className="wrap">{text}</span></span></h1>
+                  <h1>{`I'm into `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack", "Competitive Programming", "Data Science" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Third-Year Undergraduate Student at Anand International College of Engineering ,Jaipur.</p>
-                 <a href="#connect" style={{textDecoration:'none'}}><button >Let’s Connect <ArrowRightCircle size={25} /></button></a> 
-              </div>}
+                  <a href="#connect" style={{ textDecoration: 'none' }}><button >Let’s Connect <ArrowRightCircle size={25} /></button></a>
+                </div>}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-                <div className="myimgDiv">
-                  <img  className='myImg' src={meR} alt="Header Img"/>
-                </div>
+              <div className="myimgDiv">
+                <img className='myImg' src={meR} alt="Header Img" />
+                <a href="https://github.com/RUDHRAM01/resume/blob/main/RUDHRAM%20Resume.pdf" style={{ textDecoration: "none" }} download>
+
+                  <button className="resumeBtn">
+                    <span class="circle1"></span>
+                    <span class="circle2"></span>
+                    <span class="circle3"></span>
+                    <span class="circle4"></span>
+                    <span class="circle5"></span>
+                    <span class="text">Resume</span>
+                  </button>
+                </a>
+              </div>
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
+
     </section>
   )
 }
